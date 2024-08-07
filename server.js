c64a8ae0ba06875f09da6f57ffc1452defd51e7c
Sanjay Khati Chhetri <sanjaykhati579@gmail.com>
2024-08-07 13:11:08 +0300

    build: Update build command to use --configuration production flag and remove proxy configuration

--- server.js
+++ server.js
@@ -6,31 +6,19 @@ const app = express();
 app.use(compression());
 
 // Serve static files
-app.use(express.static(path.join(__dirname, "dist/todo-frontend-angular")));
+app.use(
+  express.static(path.join(__dirname, "dist/todo-frontend-angular/browser"))
+);
 
 // Send all requests to index.html
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "dist/todo-frontend-angular/index.html"));
+  res.sendFile(
+    path.join(__dirname, "dist/todo-frontend-angular/browser/index.html")
+  );
 });
 
 // Start the app by listening on the default Heroku port
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-});
-
-// Error handling
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
-
-process.on("uncaughtException", (err) => {
-  console.error("Uncaught Exception:", err);
-  process.exit(1);
-});
-
-process.on("unhandledRejection", (reason, promise) => {
-  console.error("Unhandled Rejection at:", promise, "reason:", reason);
-  process.exit(1);
 });
