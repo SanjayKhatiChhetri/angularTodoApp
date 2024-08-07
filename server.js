a4cada744842d8b3658554fa8c685ff336f91f67
Sanjay Khati Chhetri <sanjaykhati579@gmail.com>
2024-08-07 11:55:18 +0300

    fix: deployment isuue in heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const compression = require("compression");
+
+const app = express();
+app.use(compression());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "dist/todo-frontend-angular")));
+
+// Send all requests to index.html
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "dist/todo-frontend-angular/index.html"));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
